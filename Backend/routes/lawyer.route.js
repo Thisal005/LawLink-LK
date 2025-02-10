339a224bd720c0e58448d168d1ded90d020e6cd5
Thisal Thiranjith <147968575+Thisal005@users.noreply.github.com>
2025-02-11 01:05:22 +0530

    Delete Backend directory

--- Backend/routes/lawyer.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userAuth from "../middleware/userAuth.js";
-import { getLawyerData } from "../controllers/lawyer.controller.js";
-
-const lawyerRouter = express.Router();
-
-lawyerRouter.get('/data', userAuth, getLawyerData);
-
-export default lawyerRouter;
